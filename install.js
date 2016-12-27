2a695c88a5d764e5351ddcb9a66d0c06e6ed5128
Tariq Ettaji <tettaji@xebia.com>
2016-12-27 14:02:08 +0100

    bower-installer is now installed via the post-install script

--- install.js
+++ install.js
@@ -1,2 +1,6 @@
-require('child_process').exec(require('path').join('node_modules', '.bin', 'bower-installer') + ' --keep --remove');
+var exec = require('child_process').exec;
+
+exec('npm install "./bower-installer"');
+
+exec(require('path').join('node_modules', '.bin', 'bower-installer') + ' --keep --remove');
 var fs = require('fs');
